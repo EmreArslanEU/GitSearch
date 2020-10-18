ef69856bbd7b7cbdde764145148b50cf5245e646
FTS <ea0977@gmail.com>
2020-10-18 13:54:48 +0200

    uncommenting query

--- database/src/main/java/com/example/database/dao/RepoDao.kt
+++ database/src/main/java/com/example/database/dao/RepoDao.kt
@@ -16,6 +16,7 @@
 
 package com.example.database.dao
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -29,12 +30,12 @@ interface RepoDao: BaseDao<Repo> {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(repos: List<Repo>)
 
-    /*
+
     @Query("SELECT * FROM repos WHERE " +
             "name LIKE :queryString OR description LIKE :queryString " +
             "ORDER BY stars DESC, name ASC")
     fun reposByName(queryString: String): PagingSource<Int, Repo>
-*/
+
     @Query("DELETE FROM repos")
     suspend fun clearRepos()
 
