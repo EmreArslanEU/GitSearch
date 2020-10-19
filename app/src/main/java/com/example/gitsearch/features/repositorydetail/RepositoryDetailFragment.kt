a494ed2a05060bdb230c8df57b2a2f99a41fcec1
FTS <ea0977@gmail.com>
2020-10-19 23:39:03 +0200

    functionalities for deail view added with databinding

--- app/src/main/java/com/example/gitsearch/features/repositorydetail/RepositoryDetailFragment.kt
+++ app/src/main/java/com/example/gitsearch/features/repositorydetail/RepositoryDetailFragment.kt
@@ -1,12 +1,20 @@
 package com.example.gitsearch.features.repositorydetail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.navigation.navArgs
 import com.example.gitsearch.R
+import com.example.gitsearch.databinding.RepositoryDetailFragmentBinding
+import com.example.shareddtos.Repo
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,12 +25,31 @@ class RepositoryDetailFragment : Fragment() {
     }
 
     private val viewModel: RepositoryDetailViewModel by viewModels()
+    private lateinit var binding: RepositoryDetailFragmentBinding
+    private val args: RepositoryDetailFragmentArgs by navArgs()
+    private lateinit var repo:LiveData<Repo>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.repository_detail_fragment, container, false)
+        binding = RepositoryDetailFragmentBinding.inflate(inflater,container, false)
+
+        if(args.repoid == -1L)findNavController().navigateUp()
+        //binding.repo = Repo(1,"","","","",0,"","","","","")
+        repo = viewModel.getRepo(args.repoid)
+        repo.observe(viewLifecycleOwner,{
+            binding.repo = it
+
+            binding.buttonToGithub.setOnClickListener {v ->
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(it.url))
+                requireContext().startActivity(intent)
+            }
+        })
+
+
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/gitsearch/features/repositorydetail/RepositoryDetailViewModel.kt
+++ app/src/main/java/com/example/gitsearch/features/repositorydetail/RepositoryDetailViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.gitsearch.features.repositorydetail
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.example.gitsearch.repository.GithubRepository
+import com.example.shareddtos.Repo
 
-class RepositoryDetailViewModel @ViewModelInject constructor(): ViewModel() {
+class RepositoryDetailViewModel @ViewModelInject constructor( val repository : GithubRepository): ViewModel() {
+    private var repo: LiveData<Repo> = MutableLiveData()
+
+    fun getRepo(id:Long):LiveData<Repo>{
+        if(repo.value?.id== id) return repo
+        repo = repository.getRepoFromDB(id).asLiveData()
+        return repo
+    }
+
 }
